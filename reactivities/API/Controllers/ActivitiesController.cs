ee4bb5a2a4a2143bbcac3b53e1c3625a73805dcb
pak <rupaklamadev@gmail.com>
2021-11-20 23:55:37 -0500

    created activities endpoints

--- reactivities/API/Controllers/ActivitiesController.cs
+++ reactivities/API/Controllers/ActivitiesController.cs
@@ -5,27 +5,31 @@ using Microsoft.AspNetCore.Mvc;
 using Persistence;
 using Domain;
 using System;
+using Application.Activities;
+using MediatR;
 
 namespace API.Controllers
 {
   public class ActivitiesController : BaseApiController
   {
-    private readonly DataContext _context;
-    public ActivitiesController(DataContext context)
-    {
-      _context = context;
-    }
+
 
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-      return await _context.Activities.ToListAsync();
+      return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    public async Task<ActionResult<Activity>> GetActivityAsync(Guid id)
     {
-      return await _context.Activities.FindAsync(id);
+      return await Mediator.Send(new Details.Query { Id = id });
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity(Activity activity)
+    {
+      return Ok(await Mediator.Send(new Create.Command { Activity = activity }));
     }
   }
 }
--- reactivities/API/Startup.cs
+++ reactivities/API/Startup.cs
@@ -15,6 +15,9 @@ using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
+using Application.Activities;
+using MediatR;
+
 namespace API
 {
   public class Startup
@@ -50,6 +53,8 @@ namespace API
           policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
         });
       });
+
+      services.AddMediatR(typeof(List.Handler).Assembly);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
