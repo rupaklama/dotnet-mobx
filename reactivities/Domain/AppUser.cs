5ef02a06edbfd5397089a64e761fa083d539046e
pak <rupaklamadev@gmail.com>
2021-12-21 19:07:26 -0500

    added cloudinary for data storage

--- reactivities/Domain/AppUser.cs
+++ reactivities/Domain/AppUser.cs
@@ -12,6 +12,7 @@ namespace Domain
     public string Bio { get; set; }
 
     public ICollection<ActivityAttendee> Activities { get; set; }
+    public ICollection<Photo> Photos { get; set; }
 
     public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
 
